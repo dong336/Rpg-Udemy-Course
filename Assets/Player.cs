ea4df83c0ac1b3e8049f322958e2632df38e4c99
kdw <50583568+dong336@users.noreply.github.com>
2025-01-06 11:57:11 +0900

    플레이어 애니메이션

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -9,30 +9,93 @@ public class Player : MonoBehaviour
     
     [SerializeField] float moveSpeed;
     [SerializeField] float jumpForce;
-    [SerializeField] bool isMoving;
 
     private float xInput;
+    private int facingDir = 1;
+    private bool facingRight = true;
 
-    // Start is called before the first frame update
+    [Header("Collision info")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        Movement();
+
+        CheckInput();
+
+        CollisionChecks();
+
+        FlipController();
+        
+        AnimatorControllers();
+    }
+
+    private void CollisionChecks()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+
+    private void CheckInput()
+    {
         xInput = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2 (xInput * moveSpeed, rb.velocity.y);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity = new Vector2 (rb.velocity.x, jumpForce);
+            Jump();
         }
+    }
 
-        isMoving = rb.velocity.x != 0;
+    private void Movement()
+    {
+        rb.velocity = new Vector2(xInput * moveSpeed, rb.velocity.y);
+    }
 
+    private void Jump()
+    {
+        if (isGrounded) 
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    private void AnimatorControllers()
+    {
+        bool isMoving = rb.velocity.x != 0;
+
+        animator.SetFloat("yVelocity", rb.velocity.y);
         animator.SetBool("isMoving", isMoving);
+        animator.SetBool("isGrounded", isGrounded);
+    }
+
+    private void Flip()
+    {
+        facingDir = facingDir * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    private void FlipController()
+    {
+        if (rb.velocity.x > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (rb.velocity.x < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    private void OnDrawGizmos() 
+    {
+        Gizmos.DrawLine(transform.position, new Vector3(transform.position.x, transform.position.y - groundCheckDistance));
     }
 }
